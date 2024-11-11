e3fe157b2d47285df6c04f7907aa2c4a8da602f0
dykimspal401 <mike0607@hknu.ac.kr>
2024-11-11 19:31:03 +0900

    edit NOV.11

--- for_esp/hardware_monitoring/hardware_monitoring.ino
+++ for_esp/hardware_monitoring/hardware_monitoring.ino
@@ -1,5 +1,7 @@
 #include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
+// http://arduino.esp8266.com/stable/package_esp8266com_index.json 보드 추가
+// esp8266 and esp32 oled driver for ssd1306 displays 라이브러리 설치
 
 #include "Arimo_Regular_10.h"    // fonts, http://oleddisplay.squix.ch/
 #include "Arimo_Regular_12.h"
@@ -8,7 +10,14 @@
 
 SSD1306Wire display(0x3c, D5, D6);
 
-const int numRows = 5;      // 저장할 줄 수 Time / CPU Load/ CPU Temperature/ GPU Temperature / GPU Load
+const int numRows = 7;      // 저장할 줄 수 
+// 0 Time
+// 1 CPU name
+// 2 CPU Load
+// 3 CPU Temperature
+// 4 GPU name
+// 5 GPU Temperature 
+// 6 GPU Load
 const int numCols = 50;     // 각 줄의 최대 문자 수
 String data[numRows];       // 각 줄 데이터를 저장할 배열
 int currentRow = 0;         // 현재 줄 인덱스
@@ -38,19 +47,17 @@ void loop()
     {
     // clear the display
     display.clear();
-    
-    display.setFont(Arimo_Regular_12);
+    display.setFont(Arimo_Regular_16);
+    display.drawString(0, 0, data[0]); //Time
 
     char buffer[256];
-    display.drawString(0, 0, "i7-14700K / RTX4080S"); 
+    display.setFont(Arimo_Regular_10);
+    display.drawStringf(0, 16, buffer, "%s | %s",data[1], data[4]); 
 
     display.setFont(Arimo_Regular_14);
-    display.drawStringf(0, 16, buffer, "CPU: %s | %s",data[2], data[1]);
-    display.drawStringf(0, 32, buffer, "GPU: %s | %s",data[3], data[4]);
-    
-    display.setFont(Arimo_Regular_16);
-    display.drawString(0, 48, data[0]); //Time
-
+    display.drawStringf(0, 32, buffer, "CPU: %s | %s",data[3], data[2]);
+    display.drawStringf(0, 48, buffer, "GPU: %s | %s",data[5], data[6]);
+ 
     currentRow = 0;
     // write the buffer to the display
     display.display();
--- libre_hw_data_nframew/Program.cs
+++ libre_hw_data_nframew/Program.cs
@@ -9,7 +9,7 @@ class Program
     static void Main(string[] args)
     {
         // 아두이노 연결을 위한 시리얼 포트 설정
-        Console.WriteLine("Port Number (ex :COM0) :");
+        Console.WriteLine("Port Number ex) COM0 : ");
         string port_num = Console.ReadLine(); // 문자열 읽기
         Console.WriteLine("Selected Port : " + port_num);
 
@@ -26,6 +26,7 @@ class Program
         computer.Open();
         while (true) // 무한 루프 시작
         {
+            
             DateTime currentTime = DateTime.Now; // 현재 시간 가져오기
             string Time = $"{currentTime.Hour}:{currentTime.Minute}:{currentTime.Second}";
             Console.WriteLine(Time); // 콘솔 출력
@@ -33,23 +34,28 @@ class Program
 
             foreach (var hardware in computer.Hardware)
             {
-                hardware.Update();
+            hardware.Update();
+            string hardwareName = hardware.Name.Replace("Intel Core ", "").Trim(); // 문자열에서 "Intel Core" 제거
+            hardwareName = hardwareName.Replace("NVIDIA GeForce ", "").Trim(); // 문자열에서 "NVIDIA Geforce" 제거
+            Console.WriteLine(hardwareName); // 콘솔창에 출력
+            arduinoPort.WriteLine(hardwareName);// 아두이노 포트로 보내기
 
                 foreach (var sensor in hardware.Sensors)
                 {
                     
                     if (sensor.SensorType == SensorType.Temperature)
                     {
+                    //Console.WriteLine($"{sensor.Name} : {sensor.Value.GetValueOrDefault()} 'C");// 모든 데이터 출력 (테스트용)
                     if (sensor.Name == "Core Average") 
                         {
                             string cpu_core_t = $"{(int)Math.Round(sensor.Value.GetValueOrDefault())} 'C";
-                            Console.WriteLine(cpu_core_t);  // 콘솔 출력
+                            Console.WriteLine($"{sensor.Name} : {cpu_core_t}");  // 콘솔 출력
                             arduinoPort.WriteLine(cpu_core_t);  // Arduino로 전송
                         }
                         if (sensor.Name == "GPU Core") 
                         {
                             string gpu_core_t = $"{(int)Math.Round(sensor.Value.GetValueOrDefault())} 'C";
-                            Console.WriteLine(gpu_core_t);  // 콘솔 출력
+                            Console.WriteLine($"{sensor.Name} : {gpu_core_t}");  // 콘솔 출력
                             arduinoPort.WriteLine(gpu_core_t);  // Arduino로 전송
                         }
                   
@@ -57,24 +63,25 @@ class Program
 
                     if (sensor.SensorType == SensorType.Load)
                     {
-                        if (sensor.Name == "CPU Total")
+                   // Console.WriteLine($"{sensor.Name} : {sensor.Value.GetValueOrDefault()} %");// 모든 데이터 출력 (테스트용)
+                    if (sensor.Name == "CPU Total")
                         {
                             string cpu_core_l = $"{(int)Math.Round(sensor.Value.GetValueOrDefault())} %";
-                            Console.WriteLine(cpu_core_l);  // 콘솔 출력
+                            Console.WriteLine($"{sensor.Name} : {cpu_core_l}");  // 콘솔 출력
                             arduinoPort.WriteLine(cpu_core_l);  // Arduino로 전송
 
                         }
                         if (sensor.Name == "GPU Core")
                         {
                             string gpu_core_l = $"{(int)Math.Round(sensor.Value.GetValueOrDefault())} %";
-                            Console.WriteLine(gpu_core_l);  // 콘솔 출력
+                            Console.WriteLine($"{sensor.Name} : {gpu_core_l}");  // 콘솔 출력
                             arduinoPort.WriteLine(gpu_core_l);  // Arduino로 전송
                         }
                     }
                 }
             }
 
-            Thread.Sleep(100); // 0.1초 대기 (원하는 주기로 조절 가능)
+            Thread.Sleep(500); // 0.1초 대기 (원하는 주기로 조절 가능)
         }
 
         // 프로그램이 종료될 때 실행 (실제로는 도달하지 않음)
